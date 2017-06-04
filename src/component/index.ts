4188ca0618ba46810568259c6b4431bda7c4c68f
xiaozs <koumuyou@163.com>
2017-06-04 14:59:33 +0800

    com模块接口

---
+++ src/component/index.ts
@@ -0,0 +1,119 @@
+abstract class Component {
+    append() {
+
+    }
+    appendTo() {
+
+    }
+    prepend() {
+
+    }
+    prependTo() {
+
+    }
+    after() {
+
+    }
+    before() {
+
+    }
+    insertAfter() {
+
+    }
+    insertBefore() {
+
+    }
+    replaceWith() {
+
+    }
+    empty() {
+
+    }
+    remove() {
+
+    }
+    clone() {
+
+    }
+    attr() {
+
+    }
+    removeAttr() {
+
+    }
+    addClass() {
+
+    }
+    removeClass() {
+
+    }
+    toggleClass() {
+
+    }
+
+    children() {
+
+    }
+    parent() {
+
+    }
+    next() {
+
+    }
+    nextAll() {
+
+    }
+    prev() {
+
+    }
+    prevAll() {
+
+    }
+    siblings() {
+
+    }
+    on() {
+
+    }
+    off() {
+
+    }
+    one() {
+
+    }
+    trigger() {
+
+    }
+    width() {
+
+    }
+    height() {
+
+    }
+    scrollTop() {
+
+    }
+    scriollLeft() {
+
+    }
+    position() {
+
+    }
+    offset() {
+
+    }
+    css() {
+
+    }
+
+    toHTML() {
+
+    }
+    renderTo() {
+        
+    }
+
+    offsetParent() {
+
+    }
+}
