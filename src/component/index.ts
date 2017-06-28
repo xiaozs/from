7770068de321b84e2137306b6c3bb0175824cc0f
unknown <xiaozs@PKF-SW8-21.efunds-dev.local>
2017-06-28 16:50:18 +0800

    component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,60 +1,135 @@
 import { encode } from "Utils";
+/**
+ * 虚拟节点基础类
+ */
 abstract class BaseNode {
+    /**
+     * 保存虚拟节点对应的真实的dom
+     */
     private _dom: Node | null = null;
+    /**
+     * 获取虚拟节点对应的真实的dom
+     */
     get dom() {
         return this._dom;
     }
+    /**
+     * 保存虚拟节点的父节点
+     */
     private _parent: NormalNode | null = null;
+    /**
+     * 获取虚拟节点的父节点
+     */
     get parent() {
         return this._parent;
     }
+    /**
+     * 生成虚拟节点的真实dom
+     */
     protected abstract $createDom(): Node;
+    /**
+     * 将虚拟渲染成html字符串
+     */
     abstract toHtml(): string;
 }
+/**
+ * Text节点的虚拟节点
+ */
 class TextNode extends BaseNode {
+    /**
+     * Text节点的虚拟节点
+     * @param content 内容字符串
+     */
     constructor(content: string = "") {
         super();
         this._content = content;
     }
+    /**
+     * 保存节点内容字符串
+     */
     private _content: string;
+    /**
+     * 获取节点内容字符串
+     */
     get content() {
         return this._content;
     }
+    /**
+     * 设置节点内容字符串
+     */
     set content(content) {
         this._content = content;
         if (this.dom) {
             this.dom.nodeValue = content;
         }
     }
+    /**
+     * 生成虚拟节点的真实dom
+     */
     protected $createDom() {
         return document.createTextNode(this.content);
     }
-    toString() {
-        return this._content;
-    }
+    /**
+     * 将虚拟渲染成html字符串
+     */
     toHtml() {
         return encode(this._content);
     }
 }
+/**
+ * 带标签的虚拟节点
+ */
 abstract class TagNode extends BaseNode {
+    /**
+     * 标签名称
+     */
     readonly tagName: string;
-    constructor(tagName: string) {
+    /**
+     * 命名空间
+     */
+    readonly namespace: string;
+    /**
+     * 带标签的虚拟节点
+     * @param tagName 标签名称
+     * @param namespace 命名空间
+     */
+    constructor(tagName: string, namespace: string = "") {
         super();
         this.tagName = tagName;
+        this.namespace = namespace;
     }
+    /**
+     * 生成虚拟节点的真实dom
+     */
     protected $createDom(): Node {
-        return document.createElement(this.tagName);
+        if (this.namespace) {
+            return document.createAttributeNS(this.tagName, this.namespace);
+        } else {
+            return document.createElement(this.tagName);
+        }
     }
+    /**
+     * 保存子节点集合
+     */
     private _children: Children;
+    /**
+     * 获取子节点集合
+     */
     get children() {
         return this._children;
     }
 }
+/**
+ * 自闭合元素的虚拟节点
+ */
 class SelfCloseNode extends TagNode {
     toHtml(): string {
         return `<${this.tagName} />`
     }
 }
+/**
+ * 普通元素的虚拟节点
+ */
 class NormalNode extends TagNode {
     toHtml(): string {
         let tagName = this.tagName;
@@ -91,8 +166,6 @@ function nodeFactory(tagName: string) {
             return new NormalNode(tagName);
     }
 }
-
-
 
 
 
